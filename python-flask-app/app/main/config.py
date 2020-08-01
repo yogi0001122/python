db2cd81adec4cf611bb88f393cf059162d0c7517
Yogendra Kumar <yk185029@gmail.com>
2020-08-01 23:44:18 +0530

    yogi0001122
    New code

--- python-flask-app/app/main/config.py
+++ python-flask-app/app/main/config.py
@@ -1,7 +1,7 @@
 import os
 
 # uncomment the line below for postgres database url from environment variable
-#postgres_local_base = 'postgresql://yogi:password@Host:5432/flaskdev'
+postgres_local_base = 'postgresql://yogi:password@Host:5432/flaskdev'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
